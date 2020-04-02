659f1e97862b8b721ece069943cfe27d6f728137
AKTasos <tasos84@hotmail.com>
2020-04-02 15:36:21 +0200

    working on descriptive stats

--- abstraction-and-reasoning-challenge/beginning.py
+++ abstraction-and-reasoning-challenge/beginning.py
@@ -14,6 +14,7 @@ import json
 import matplotlib.pyplot as plt
 from matplotlib import colors
 from pathlib import Path
+from torch.utils.data import Dataset
 
 path = os.path.dirname(os.path.abspath(__file__))
 for dirname, _, filenames in os.walk(path):
@@ -29,19 +30,28 @@ training_tasks = sorted(os.listdir(training_path))
 
 
 
-def dataloader(training_tasks):
-    train_task =[]
-    for task in training_tasks :
-        train_task_file = str(training_path / task)
-        with open(train_task_file, 'r') as f:
-            train_task.append(json.load(f))
+def data_openner(tasks, path):
+    task_list =[]
+    for task in tasks :
+        task_file = str(path / task)
+        with open(task_file, 'r') as f:
+            task_list.append(json.load(f))
             
-    return train_task
+    return task_list
 
 
 
-train_task = dataloader(training_tasks)      
+train_task = data_openner(training_tasks, training_path)      
 
+
+def data_process(task_list):
+    
+
+class ARCDataset(dataset):
+    
+    def __init__(self, task_list):
+        
+    
 
 
      
@@ -50,21 +60,37 @@ class TaskParameters():
     def __init__(self, task): # task = train_task[x]          ['train'][0]['input']
         self.task = task
         self.nb_of_train = len(task['train'])
-        self.train_dict = {}
+        self.train_list = []
         
         
     def train_params(self):
         
-        n=97
+        
         for train in self.task['train']:
-            
-               self.train_dict[chr(n)] = TrainParameters(train)
-               n += 1
+            data = TrainParameters(train)
+            data.colors_params()
+            self.train_list.append(data.__dict__)
+          
                
     def size_rules(self):
-        print('a') 
+        self.train_list
+        
+        self.train_data = train_data
+        self.x_in = len(train_data['input'][0])
+        self.x_out = len(train_data['output'][0])
+        self.y_in = len(train_data['input'])
+        self.y_out = len(train_data['output'])
+        self.ratio_in = self.x_in / self.y_in
+        self.ratio_out =self.x_out / self.y_out
+        
     def colors_rules(self):
-        print('b')
+        
+        self.colors_in = None
+        self.colors_out = None
+        self.nb_of_colors_in = None
+        self.nb_of_colors_out = None
+        self.case_by_color_in = str()
+        self.case_by_color_out = str()
         
         
         
@@ -78,31 +104,53 @@ class TrainParameters(): # train_data = train_task[0]['train'][0]       ['input'
         self.y_out = len(train_data['output'])
         self.ratio_in = self.x_in / self.y_in
         self.ratio_out =self.x_out / self.y_out
-        self.colors_in = None
-        self.colors_out = None
-        self.nb_of_colors_in = None
-        self.nb_of_colors_out = None
-        self.case_by_color_in = str()
-        self.case_by_color_out = str()
+        # self.colors_in = None
+        # self.colors_out = None
+        # self.nb_of_colors_in = None
+        # self.nb_of_colors_out = None
+        self.case_by_color_in = []
+        self.case_by_color_out = []
         
         
     def colors_params(self):
         flat_in = [item for sublist in self.train_data['input'] for item in sublist]
         flat_out = [item for sublist in self.train_data['output'] for item in sublist]
         
-        self.colors_in = ''.join(map(str, sorted(set(flat_in))))
-        self.colors_out = ''.join(map(str, sorted(set(flat_in))))
+        # self.colors_in = ''.join(map(str, sorted(set(flat_in))))
+        # self.colors_out = ''.join(map(str, sorted(set(flat_in))))
         
-        self.nb_of_colors_in = len(set(flat_in))
-        self.nb_of_colors_out = len(set(flat_out))
+        # self.nb_of_colors_in = len(set(flat_in))
+        # self.nb_of_colors_out = len(set(flat_out))
         
         for x in range(10): 
-            self.case_by_color_in += str(flat_in.count(x))
-            self.case_by_color_out += str(flat_out.count(x))
+            self.case_by_color_in.append(flat_in.count(x))
+            self.case_by_color_out.append(flat_out.count(x))
             
-            
+    # def return_parameters(self):
+        
+        
             # flat_in.count(x) = ''.join(map(str, flat_in)))
             # flat_out.count(x) = ''.join(map(str, flat_out)))
             
-            
+      def color_distrib(self):          
+          
+          
+          
+          
+          
+          
+          # list version
+          # for color in range(10):
+          #     for y in range(len(self.train_data)):
+          #         try : 
+          #             n = y.count(color)
+          #             for i in range(n)
+          #                 y.index(color)
+          #         except ValueError:
+          #             pass
+                      
+                  
+          #         for x in range (len(self.train_data[y])):
+                      
+  
         
---
+++ abstraction-and-reasoning-challenge/networkconv.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 14:29:19 2020
+
+@author: aktasos
+"""
+import torch.nn as nn
+# import torch.nn.functional as F
+import math
+
+class CnnFcNetwork(nn.Module):
+    def __init__(self, images, kernel_size, nb_of_fclayers, out_feat):
+        
+        self.in_channels = images.shape[1]
+        self.img_size = images.shape[2]*images.shape[3]
+        self.new_feat_map = self.in_channels*2
+        self.kernel_size = kernel_size
+        self.layer_output = images.shape[2]
+        self.n_delta = None
+        self.out_features = None
+        self.output = out_feat
+        self.nb_of_fclayers = nb_of_fclayers
+        
+        # super function. It inherits from nn.Module and we can access everythink in nn.Module
+        super(CnnFcNetwork,self).__init__()
+        # Function.
+    # determine number of convolution according to size of image
+        self.nb_of_conv = int(math.log(self.img_size)/math.log(2))
+     
+    # creating layers
+        
+        self.cnn = []
+        for n in range(self.nb_of_conv) :
+            self.cnn += [nn.Conv2d(self.in_channels, self.new_feat_map, self.kernel_size),
+                    nn.BatchNorm2d(self.new_feat_map),
+                    nn.LeakyReLU()]
+            
+            self.in_channels = self.new_feat_map
+            self.new_feat_map = self.new_feat_map*2
+            self.layer_output = (self.layer_output-self.kernel_size)+1
+        
+        self.cnn = nn.Sequential(*self.cnn)
+        
+        self.layer_output = int(self.layer_output ** 2 * (self.new_feat_map/2))
+        self.n_delta = self.layer_output // nb_of_fclayers
+        self.out_features = self.layer_output - self.n_delta
+        
+        
+        self.fc = []
+        for n in range(self.nb_of_fclayers) :
+            if n == self.nb_of_fclayers-1 :
+                self.out_features = self.output
+            self.fc += [nn.Linear(in_features = self.layer_output, out_features = self.out_features)
+                        , nn.LeakyReLU()]     
+            self.layer_output -= self.n_delta
+            self.out_features -= self.n_delta
+            
+        self.fc = nn.Sequential(*self.fc)
+
+
+
+    def forward(self,x):
+        x = self.cnn(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+    
