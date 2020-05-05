31ac626c7c23e940bd0576125b7a627ba3f5c8e3
AKTasos <tasos84@hotmail.com>
2020-05-05 16:00:18 +0200

    detect grids and borders

--- abstraction-and-reasoning-challenge/tools.py
+++ abstraction-and-reasoning-challenge/tools.py
@@ -65,44 +65,48 @@ def detect_borders(matrix):
         return max_color, border_dict       
     
 
-def detect_lines(matrix):
+def detect_grids(matrix):
     lines = []
     columns = []
+    # temp_mat_lines = []
+    # temp_mat_columns = []
+    grid = False
     for index, line in enumerate(matrix[0][0]):
-        
         unique_values, count = line.unique(return_counts=True)
-        
-        
-        if list(unique_values.size())==[1]:
-            lines.append(index)
-        if max(count)<    
-        
-    for index, line in enumerate(matrix[0][0].transpose(0,1)):
-        if list(line.unique().size())==[1]:
-            columns.append(index)    
-        
-    up, u_count =  matrix[0][0][0].unique(return_counts=True)
-    right, r_count= matrix[0][0].transpose(0,1)[0].unique(return_counts=True)
-    down, d_count = matrix[0][0][-1].unique(return_counts=True)
-    left, l_count = matrix[0][0].transpose(0,1)[-1].unique(return_counts=True)
-    values = torch.cat((up, right,down,left))
-    unique_values = values.unique()
-    count = torch.cat((u_count, r_count,d_count,l_count))
-    border_dict = {}
-    for uv in unique_values:
-        border_dict[int(uv)] = 0 
-        indices = np.where(values==int(uv))[0]
-        for i in indices:
-            border_dict[int(uv)] += int(count[i])
+        if len(unique_values)==1:
+            lines.append((index, int(unique_values)))
+        # else:
+        #     temp_mat_lines.append(line)
             
-    max_color = max(border_dict, key=border_dict.get)
-    if border_dict[max_color] < (np.ma.size(matrix)/2):
-        return None, border_dict
-    else:
-        return max_color, border_dict
+    for index, column in enumerate(matrix[0][0].transpose(0,1)):
+        if len(unique_values)==1:
+            columns.append((index, int(unique_values)))  
+         # else:
+         #        temp_mat_columns.append(columns)
+            
+    
+    # if lines:
+    #     temp_mat_lines = np.array(temp_mat_lines).transpose(0,1)
+    #     for index, line in enumerate(temp_mat):
+    #         if len(unique_values)==1:
+    #             columns.append(index)  
+                            
+    
+    
+    
+    if lines and columns:
+        grid = True
+        
+        
+        return lines, columns, grid
+    
+    
 
-
+ # elif max(count) >= (len(lines)/3)*2 and len(unique_values)<=3:
+ #            lines.append(index)
 
 
 max_c1, b1 = detect_borders(input1)
 max_c2, b2 = detect_borders(input2)
+lines1, columns1, grid1 = detect_grids(input1)
+lines2, columns2, grid2 = detect_grids(input2)
