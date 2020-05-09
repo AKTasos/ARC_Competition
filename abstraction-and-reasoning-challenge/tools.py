bf718da92f2f933ec3db09bceb9ae6a8e3e8b6ce
AKTasos <tasos84@hotmail.com>
2020-05-09 18:25:43 +0200

    sub matrix decomposition works

--- abstraction-and-reasoning-challenge/tools.py
+++ abstraction-and-reasoning-challenge/tools.py
@@ -78,11 +78,11 @@ def detect_grids(matrix):
     for index, line in enumerate(matrix):
         unique_values = line.unique()
         if len(unique_values) == 1:
-            lines.append((index, int(unique_values)))
+            lines.append([index, int(unique_values)])
     for index, column in enumerate(matrix.transpose(0, 1)):
         unique_values = column.unique()
         if len(unique_values) == 1:
-            columns.append((index, int(unique_values)))
+            columns.append([index, int(unique_values)])
     if lines and columns:
         grid = True
     return lines, columns, grid
@@ -94,48 +94,62 @@ def background(matrix):
         bg, border_dict = detect_borders(matrix)
     return bg
 
-def trim_borders(matrix):
-    bg = background(matrix)
-    lines, columns, grid = detect_grids(matrix)
+def trim_borders(matrix, bg, lines, columns):
     nb_of_lines, nb_of_columns = matrix.size()
     trimed_mat = matrix
     trim = []
     lines_trim = []
+    trimed_lines = np.empty(0)
+    trimed_columns = np.empty(0)
     columns_trim = []
-    if lines[0][0] == 0 and lines[0][1] == bg:
+    n = 0
+    if lines:
+        if lines[0][0] == 0 and lines[0][1] == bg:
+            while lines[n][1] == bg and lines[n][0] == n:
+                trim.append(n)
+                lines_trim.append(n)
+                n += 1
+                if n == len(lines):
+                    break
+        if lines[-1][0] == nb_of_lines-1 and lines[-1][1] == bg:
+            m = len(lines)-1
+            last_line = nb_of_lines-1
+            while lines[m][1] == bg and lines[m][0] == last_line:
+                trim.append(last_line)
+                lines_trim.append(m)
+                last_line -= 1
+                m -= 1  
+        trimed_mat = np.delete(trimed_mat, trim, axis=0)
+        trimed_lines = np.delete(lines, lines_trim, axis=0)
+        for line in trimed_lines:
+            line[0] -= n
+            
+        trim = []
         n = 0
-        while lines[n][1] == bg and lines[n][0] == n:
-            trim.append(n)
-            lines_trim.append(n)
-            n += 1
-    if lines[-1][0] == nb_of_lines-1 and lines[-1][1] == bg:
-        n = len(lines)-1
-        last_line = nb_of_lines-1
-        while lines[n][1] == bg and lines[n][0] == last_line:
-            trim.append(last_line)
-            lines_trim.append(n)
-            last_line -= 1
-            n -= 1  
-    trimed_mat = np.delete(trimed_mat, trim, axis=0)
-    trimed_lines = np.delete(lines, lines_trim, axis=0)
-    trim = []
-    if columns[0][0] == 0 and columns[0][1] == bg:
-        n = 0
-        while columns[n][1] == bg and columns[n][0] == n:
-            trim.append(n)
-            columns_trim.append(n)
-            n += 1
-    if columns[-1][0] == nb_of_columns-1 and columns[-1][1] == bg:
-        n = len(columns)-1
-        last_columns = nb_of_columns-1
-        while columns[n][1] == bg and columns[n][0] == last_columns:
-            trim.append(last_columns)
-            columns_trim.append(n)
-            last_columns -= 1
-            n -= 1  
-    trimed_mat = np.delete(trimed_mat.transpose(0, 1), trim, axis=0).transpose(0, 1)
-    trimed_columns = np.delete(columns, columns_trim, axis=0)
-
+    if columns:    
+        if columns[0][0] == 0 and columns[0][1] == bg:
+            while columns[n][1] == bg and columns[n][0] == n:
+                trim.append(n)
+                columns_trim.append(n)
+                n += 1
+                if n == len(columns):
+                    break
+        if columns[-1][0] == nb_of_columns-1 and columns[-1][1] == bg:
+            m = len(columns)-1
+            last_columns = nb_of_columns-1
+            while columns[m][1] == bg and columns[m][0] == last_columns:
+                trim.append(last_columns)
+                columns_trim.append(m)
+                last_columns -= 1
+                m -= 1  
+        trimed_mat = np.delete(trimed_mat.transpose(0, 1), trim, axis=0).transpose(0, 1)
+        trimed_columns = np.delete(columns, columns_trim, axis=0)
+        for column in trimed_columns:
+            column[0] -= n
+    
+    if trimed_mat.size() == matrix.size():
+        trimed_mat, trimed_lines, trimed_columns = None, None, None
+        
     return trimed_mat, trimed_lines, trimed_columns
 
 def sub_matrices(matrix, lines, columns):
@@ -150,23 +164,6 @@ def sub_matrices(matrix, lines, columns):
             if np.count_nonzero(m) != 0:
                 sub_m.append(m)
     return sub_m
-            
-        
-    
-    
-    # n = 0
-    # while lines[n][0] == n:
-    #     del lines[0]
-    #     n += 1
-    #     cuts.append(n)
-    
-    # if columns[-1][0] == nb_of_columns-1  
-    
-    # if lines[0][0] == 0 and lines[0][1] == bg:
-    #     n = 0
-    #     while lines[n][1] == bg and lines[n][0] == n:
-    #         trim.append(n)
-    #         n += 1
 
 def plot_grid(mat, vmin=0, vmax=9):
     cmap = colors.ListedColormap(
@@ -181,18 +178,30 @@ def plot_grid(mat, vmin=0, vmax=9):
 class MatrixDecomposition():
     def __init__(self, matrix):
         self.matrix = matrix[0][0]
-        self.decomposition = []
         self.color, self.border_dict = detect_borders(self.matrix)
         self.bg = background(self.matrix)
         self.lines, self.columns, self.grid = detect_grids(self.matrix)
-        self.trimed_mat, self.trimed_lines, self.trimed_columns = trim_borders(self.matrix)
-        self.sub_m = sub_matrices(self.trimed_mat, self.trimed_lines, self.trimed_columns)
-        # for mat in sub_m
-        #     self.color, self.border_dict = detect_borders(self.matrix)
-        #     self.bg = background(self.matrix)
-        #     self.lines, self.columns, self.grid = detect_grids(self.matrix)
-        #     self.trimed_mat, self.trimed_lines, self.trimed_columns = trim_borders(self.matrix)
-        #     self.sub_m = sub_matrices(self.trimed_mat, self.trimed_lines, self.trimed_columns)
+        self.all_mat = [self.matrix]
+        if self.lines or self.columns:
+            self.trimed_mat, self.trimed_lines, self.trimed_columns = trim_borders(self.matrix, self.bg, self.lines, self.columns)
+            self.all_mat.append(self.trimed_mat)
+            if self.trimed_lines.size > 0 or self.trimed_columns.size > 0:
+                self.sub_m = sub_matrices(self.trimed_mat, self.trimed_lines, self.trimed_columns)
+                
+                for mat in self.sub_m:
+                    self.sub_lines, self.sub_columns, self.sub_grid = detect_grids(mat)
+                    self.sub_trimed_mat, self.sub_trimed_lines, self.sub_trimed_columns = trim_borders(mat, self.bg, self.sub_lines, self.sub_columns,)
+                    self.all_mat.append(self.sub_trimed_mat)
+                    if self.sub_trimed_lines.size > 0 or self.sub_trimed_columns.size > 0:
+                        self.sub_sub_m = sub_matrices(self.trimed_mat, self.trimed_lines, self.trimed_columns)
+                        for smat in self.sub_sub_m: 
+                            self.ssub_lines, self.ssub_columns, self.ssub_grid = detect_grids(smat)
+                            self.ssub_trimed_mat, self.ssub_trimed_lines, self.ssub_trimed_columns = trim_borders(smat, self.bg, self.ssub_lines, self.ssub_columns)
+                            self.all_mat.append(self.ssub_trimed_mat)
+
+        
+
+
 a = MatrixDecomposition(input1)       
 b = MatrixDecomposition(input2)
 c = MatrixDecomposition(input3)
